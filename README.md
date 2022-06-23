9426e0c8aba0d442742b4088d195d87d83b696ee
Frank <jssmith883@gmail.com>
2022-06-24 02:52:48 +0300

    Finish a Guess Game

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ TODO List
 - [x] 安装 Rust 开发环境
 - [x] 写一个 Hello World
 - [x] 使用 Cargo 外加 ferris-says 依赖
-- [ ] 编写猜猜看游戏
+- [x] 编写猜猜看游戏 使用 `cargo run --bin guessing_game` 运行
 
---
+++ src/bin/guessing_game.rs
@@ -0,0 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Equal => {
+                println!("Congratulations! You Win!");
+                break;
+            },
+        }
+    }
+}
